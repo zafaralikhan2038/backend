512f3ec7447ff45648a53d73eea3cccd3db6a6e6
Zafar Ali Khan <zafarak147@gmail.com>
2026-01-08 18:11:23 +0530

    connection of mongodb

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import express from "express";
+const app=express();
+
+dotenv.config({
+    path: './.env'
+});
+
+connectDB();
+
+
+
+
+
+
+
+// const connectDB=async()=>{
+//     try{
+//        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//        app.on("error",(error)=>{
+//         console.log("ERRR:",error);
+//          throw error;
+//          });
+
+//          app.listen(process.env.PORT,()=>{
+//             console.log(`App is listening on port ${process.env.PORT}`);
+//          });
+            
+        
+//     }catch(error){
+//         console.log("Error connecting to MongoDB", error);
+//         throw error;
+//     }
+// };
+
+// connectDB();
